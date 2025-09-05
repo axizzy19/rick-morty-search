a3d850344e92c0c3853cf8f3dcdf4c2a58c05284
Alexandra Akhundova <alexsandra.akhundova@yandex.ru>
2025-09-05 16:15:28 +0300

    api settings are changed

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,7 +5,7 @@ import type {
   Episode,
 } from '../types';
 
-const BASE_URL = 'https://rickandmortyapi.com/api';
+const BASE_URL = import.meta.env.BASE_URL;
 
 export const api = {
   async getCharacters(
